28d6c833c2d830d8bf3264b9da3094216c2eaf80
prime-infinity <soe.a.prime@gmail.com>
2023-05-21 11:51:21 -0700

    done first page with puppeteer, gone to google

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 import puppeteer from "puppeteer";
+(async () => {
+  //start
+
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  await page.goto("https://google.com");
+  await browser.close();
+  console.log("working");
+})();
