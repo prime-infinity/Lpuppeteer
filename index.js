79510082b919321db7a71030eb761dca30ec510b
prime-infinity <soe.a.prime@gmail.com>
2023-05-21 15:31:21 -0700

    clicking and navigations

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ import puppeteer from "puppeteer";
   });
   const page = await browser.newPage();
   await page.goto("https://google.com");
+
+  const title = await page.title();
+  console.log("title", title);
+
+  //const h1 = await page.$eval('h1', el => el.textContent)
+  //const dataPoints = await page.$$('.class-one.class-two.class-three')
+
+  /*for (let i = 0; i < array.length; i++) {
+      const inside = array[i];
+      console.log('inside element', await inside.$eval('span',el => el.textContent));
+  }*/
+
+  //await page.click('[href="https://google.com/signin"]')
+
+  //await page.waitForNavigation()
+
+  //await page.waitForSelector('.class-to-wait-for')
+
   await browser.close();
   console.log("working");
 })();
