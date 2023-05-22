adc6881b51e72492d9f9ed067814452195eb2494
prime-infinity <soe.a.prime@gmail.com>
2023-05-22 06:13:35 -0700

    done basic puppeteer concepts

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ import puppeteer from "puppeteer";
 
   const browser = await puppeteer.launch({
     headless: false,
+    //slowMo:100
   });
   const page = await browser.newPage();
   await page.goto("https://google.com");
 
-  const title = await page.title();
-  console.log("title", title);
+  //const title = await page.title();
+  //console.log("title", title);
 
   //const h1 = await page.$eval('h1', el => el.textContent)
   //const dataPoints = await page.$$('.class-one.class-two.class-three')
@@ -24,6 +25,13 @@ import puppeteer from "puppeteer";
   //await page.waitForNavigation()
 
   //await page.waitForSelector('.class-to-wait-for')
+
+  //another way of clicking
+  /*const element = await page.$('.class-to-click')
+  await page.evaluate(element, element.click(), element)*/
+
+  /*await page.type("#APjFqb", "string to type");
+  await page.waitForTimeout(1000);*/
 
   await browser.close();
   console.log("working");
